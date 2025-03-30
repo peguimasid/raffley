be6d0874b3aec1cdbfcffed85a3b679dafa84202
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-30 13:03:55 -0300

    create first livevie

---
+++ lib/raffley_web/live/estimator_live.ex
@@ -0,0 +1,30 @@
+defmodule RaffleyWeb.EstimatorLive do
+  use RaffleyWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket = assign(socket, tickets: 0, price: 3)
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div class="estimator">
+      <h1>Raffle Estimator</h1>
+      <section>
+        <div>
+          {@tickets}
+        </div>
+        @
+        <div>
+          ${@price}
+        </div>
+        =
+        <div>
+          ${@tickets * @price}
+        </div>
+      </section>
+    </div>
+    """
+  end
+end
