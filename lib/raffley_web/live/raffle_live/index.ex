7ab379be6905672494ee82e805dab0e480f75d6e
Guilhermo Masid <guilhermomasid@gmail.com>
2025-04-02 19:26:11 -0300

    function compoenents

--- lib/raffley_web/live/raffle_live/index.ex
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -12,15 +12,33 @@ defmodule RaffleyWeb.RaffleLive.Index do
     ~H"""
     <div class="raffle-index">
       <div class="raffles">
-        <div :for={raffle <- @raffles} class="card">
-          <img src={raffle.image_path} alt={raffle.description} />
-          <h2>{raffle.prize}</h2>
-          <div class="details">
-            <div class="price">${raffle.ticket_price} / ticket</div>
-            <div class="badge">{raffle.status}</div>
-          </div>
-        </div>
+        <.raffle_card :for={raffle <- @raffles} raffle={raffle} />
       </div>
+    </div>
+    """
+  end
+
+  attr :raffle, Raffley.Raffle, required: true
+
+  def raffle_card(assigns) do
+    ~H"""
+    <div class="card">
+      <img src={@raffle.image_path} alt={@raffle.description} />
+      <h2>{@raffle.prize}</h2>
+      <div class="details">
+        <div class="price">${@raffle.ticket_price} / ticket</div>
+        <.badge status={@raffle.status} />
+      </div>
+    </div>
+    """
+  end
+
+  attr :status, :atom, required: true, values: [:upcoming, :open, :closed]
+
+  def badge(assigns) do
+    ~H"""
+    <div class="rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border text-lime-600 border-lime-600">
+      {@status}
     </div>
     """
   end
