1fd964b37d86b4e2a23bb6b4bab58e8fdbc3a4c9
Guilhermo Masid <guilhermomasid@gmail.com>
2025-04-11 19:28:41 -0300

    filter raffles using ecto queries

--- lib/raffley/raffles.ex
+++ lib/raffley/raffles.ex
@@ -1,9 +1,18 @@
 defmodule Raffley.Raffles do
   alias Raffley.Repo
   alias Raffley.Raffles.Raffle
+  import Ecto.Query
 
   def list_raffles do
     Raffle
+    |> Repo.all()
+  end
+
+  def filter_raffles() do
+    Raffle
+    |> where(status: :closed)
+    |> where([r], ilike(r.prize, "%gourmet%"))
+    |> order_by(:prize)
     |> Repo.all()
   end
 
@@ -13,6 +22,10 @@ defmodule Raffley.Raffles do
   end
 
   def featured_raffles(%Raffle{id: id}) do
-    Enum.filter(list_raffles(), fn raffle -> raffle.id != id end)
+    Raffle
+    |> where([r], r.id != ^id)
+    |> order_by(desc: :ticket_price)
+    |> limit(3)
+    |> Repo.all()
   end
 end
