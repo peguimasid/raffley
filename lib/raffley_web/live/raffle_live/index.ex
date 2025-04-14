2f33d9fdbb45e56535cb19ec78f971b02a7e453c
Guilhermo Masid <guilhermomasid@gmail.com>
2025-04-14 19:00:14 -0300

    filter form layout

--- lib/raffley_web/live/raffle_live/index.ex
+++ lib/raffley_web/live/raffle_live/index.ex
@@ -5,7 +5,11 @@ defmodule RaffleyWeb.RaffleLive.Index do
   alias Raffley.Raffles
 
   def mount(_params, _session, socket) do
-    socket = stream(socket, :raffles, Raffles.list_raffles())
+    socket =
+      socket
+      |> stream(:raffles, Raffles.list_raffles())
+      |> assign(:form, to_form(%{}))
+
     {:ok, socket}
   end
 
@@ -21,10 +25,31 @@ defmodule RaffleyWeb.RaffleLive.Index do
           Any guesses?
         </:details>
       </.banner> --%>
+      <.filter_form form={@form} />
       <div class="raffles" id="raffles" phx-update="stream">
         <.raffle_card :for={{dom_id, raffle} <- @streams.raffles} raffle={raffle} id={dom_id} />
       </div>
     </div>
+    """
+  end
+
+  def filter_form(assigns) do
+    ~H"""
+    <.form for={@form}>
+      <.input field={@form[:q]} placeholder="Search..." autocomplete="off" class="leading-3" />
+      <.input
+        type="select"
+        field={@form[:status]}
+        prompt="Status"
+        options={[:upcoming, :open, :closed]}
+      />
+      <.input
+        type="select"
+        field={@form[:sort_by]}
+        prompt="Sort by"
+        options={[:prize, :ticket_price]}
+      />
+    </.form>
     """
   end
 
