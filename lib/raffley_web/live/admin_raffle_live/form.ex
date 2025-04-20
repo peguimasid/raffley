b5575eb4cad5349c64f1f119036888bedbc5fc6d
Guilhermo Masid <guilhermomasid@gmail.com>
2025-04-20 09:48:30 -0300

    live form validations

--- lib/raffley_web/live/admin_raffle_live/form.ex
+++ lib/raffley_web/live/admin_raffle_live/form.ex
@@ -5,7 +5,7 @@ defmodule RaffleyWeb.AdminRaffleLive.Form do
   use RaffleyWeb, :live_view
 
   def mount(_params, _session, socket) do
-    changeset = Raffle.changeset(%Raffle{}, %{})
+    changeset = Admin.change_raffle(%Raffle{})
 
     socket =
       socket
@@ -20,9 +20,9 @@ defmodule RaffleyWeb.AdminRaffleLive.Form do
     <.header>
       {@page_title}
     </.header>
-    <.simple_form for={@form} id="raffle-form" phx-submit="save">
+    <.simple_form for={@form} id="raffle-form" phx-submit="save" phx-change="validate">
       <.input field={@form[:prize]} label="Prize" />
-      <.input field={@form[:description]} label="Description" type="textarea" />
+      <.input field={@form[:description]} label="Description" type="textarea" phx-debounce="blur" />
       <.input field={@form[:ticket_price]} label="Ticket price" type="number" />
       <.input
         field={@form[:status]}
@@ -40,6 +40,12 @@ defmodule RaffleyWeb.AdminRaffleLive.Form do
       Back
     </.back>
     """
+  end
+
+  def handle_event("validate", %{"raffle" => raffle_params}, socket) do
+    changeset = Admin.change_raffle(%Raffle{}, raffle_params)
+    socket = assign(socket, :form, to_form(changeset, action: :validate))
+    {:noreply, socket}
   end
 
   def handle_event("save", %{"raffle" => raffle_params}, socket) do
