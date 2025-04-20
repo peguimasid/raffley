e0e75feb309bd4acffa21f8f87b0fff7b55832d9
Guilhermo Masid <guilhermomasid@gmail.com>
2025-04-20 17:23:30 -0300

    ecto one to many association from charity to raffles

--- .iex.exs
+++ .iex.exs
@@ -2,4 +2,7 @@ alias Raffley.Repo
 alias Raffley.Raffles
 alias Raffley.Raffles.Raffle
 alias Raffley.Admin
+alias Raffley.Charities
+alias Raffley.Charities.Charity
 import Ecto.Query
+import Ecto.Changeset
